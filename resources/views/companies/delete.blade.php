1d0693ee96890c95256c2ecce9eb0725daabaa32
Bec <70154628+BecWy@users.noreply.github.com>
2021-07-19 19:10:41 +0100

    Updated companies modals functionality and styling

--- resources/views/companies/delete.blade.php
+++ resources/views/companies/delete.blade.php
@@ -1,40 +1,32 @@
-<x-app-layout>
-  
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Companies') }}
-        </h2>
-    </x-slot>
+<div class="h-screen w-screen flex justify-center items-center fixed inset-0 z-50"> 
+    <div class="bg-white border rounded-lg w-7/12 max-w-screen-md max-h-full overflow-y-auto">
+        <div class="flex flex-col items-center bg-white my-4 mx-4" >
 
 
-    <div class="h-screen w-screen flex justify-center items-center fixed inset-0 z-50"> 
-        <div class="bg-gray-100 border-2 w-11/12 max-w-screen-md max-h-full overflow-y-auto">
-            <div class="flex flex-col items-center   border bg-white my-4 mx-4" >
-                <h2 class="text-lg font-bold my-4">Delete Company?</h2>
+            <h2 class="text-lg font-bold my-4">Delete Company?</h2>
 
-                <a href="/companies" class="absolute self-end px-4 py-2 text-lg"><i class="fas fa-times"></i></a>
+            <a onclick="hideDeleteModal()" class="absolute self-end px-4 py-2 text-lg cursor-pointer"><i class="fas fa-times"></i></a>
 
-                @auth
-                    <form method="POST" 
-                            action="/companies/{{$company->id}}/destroy" 
-                            class="w-9/12" 
-                            enctype="multipart/form-data"> 
-                        @csrf
+            @auth
+                <form method="POST" 
+                        action="/companies/{{$company->id}}/destroy" 
+                        class="w-10/12" 
+                        enctype="multipart/form-data"> 
+                    @csrf
 
-                        <p>Are you sure you want to delete {{$company->name}}?</p>
+                    <p>Are you sure you want to delete {{$company->name}} and all of its employees?</p>
 
 
-                        <!-- <button type="button" class="cancelbtn">Cancel</button> -->
+                    <!-- <button type="button" class="cancelbtn">Cancel</button> -->
 
-                        <div id="button-div" class="my-4 mt-8 bg-indigo-700 text-white text-center">
-                            <button type="submit" id="submit-button" class="font-bold uppercase w-full h-full py-3"> 
-                                Delete
-                            </button>
-                        </div>
-                    </form>
-                @endauth
+                    <div id="button-div" class="my-4 mt-8 bg-indigo-700 text-white text-center">
+                        <button type="submit" id="submit-button" class="font-bold uppercase w-full h-full py-3"> 
+                            Delete
+                        </button>
+                    </div>
+                </form>
+            @endauth
 
-            </div>
         </div>
     </div>
-</x-app-layout>
+</div>
--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -6,63 +6,65 @@
         </h2>
     </x-slot>
 
-    <div class="mx-auto m-w-full overflow-x-auto">
 
+    <div class="mx-auto m-w-full">
         <div class="flex flex-row items-end justify-end">
-            <a href="/companies/create" class="text-3xl" p-1><i class="far fa-plus-square"></i></a>
+            <a onclick="showCreateModal()" class="text-3xl cursor-pointer" p-1><i class="far fa-plus-square"></i></a>
         </div>
 
-        <table class="my-4 py-4 bg-white m-w-full w-full">
-            <tr>
-                <th class="bg-indigo-100 border text-left px-4 py-2"></th>
-                <th class="bg-indigo-100 border text-left px-4 py-2">Name</th>
-                <th class="bg-indigo-100 border text-left px-4 py-2">Email</th>
-                <th class="bg-indigo-100 border text-left px-4 py-2">Logo</th>
-                <th class="bg-indigo-100 border text-left px-4 py-2">Website</th>
-                <th class="bg-indigo-100 border text-left px-4 py-2">Actions</th>
-            </tr>
-            
-
-            <?php
-            $id = 0;
-            ?> 
-
-            @foreach ($companies as $company)
-            <?php
-                $id += 1;
-                $currentPage =  request()->get('page');
-
-                if($currentPage) {
-                    $offset = ($currentPage - 1) * 10;
-                } else {
-                    $offset = 0;
-                }
+        <div class="overflow-x-auto md:overflow-x-hidden w-full mb-4 mt-2">
+            <table class="my-4 py-4 bg-white m-w-full w-full">
+                <tr>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2"></th>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2">Name</th>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2">Email</th>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2">Logo</th>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2">Website</th>
+                    <th class="bg-indigo-900 text-white tracking-wider font-semibold text-left px-4 py-2">Actions</th>
+                </tr>
                 
-                $calculatedID = $id + $offset; 
-            ?>
 
-            <tr>
-                <td class="border px-4 py-2 align-top text-sm">{{$calculatedID;}}</td>
-                <td class="border px-4 py-2 align-top text-sm">{{$company->name;}}</td>
-                <td class="border px-4 py-2 align-top text-sm">{{$company->email;}}</td>
-                <td class="border px-4 py-2 align-top text-sm">
-                    @if ($company->logo)
-                        <img src="{{ $company->logo }}" alt="image" class="h-11 h-11"></img>
-                    @endif()
-                </td>
-                <td class="border px-4 py-2 align-top text-sm">{{$company->website;}}</td>
-                <td class="border px-4 py-2 align-top text-sm">
-                    <a href="/companies/{{$company->id}}/edit" class="pr-2"><i class="far fa-edit"></i></a>
-                    <a href="/companies/{{$company->id}}/delete"><i class="far fa-trash-alt"></i></a>
+                <?php
+                $id = 0;
+                ?> 
 
-                    <!-- <button id="modal-button" type="button">
-                        Launch demo modal
-                    </button> -->
-                </td>
-            </tr>
-            @endforeach
-      
-        </table>
+                @foreach ($companies as $company)
+                <?php
+                    $id += 1;
+                    $currentPage =  request()->get('page');
+
+                    if($currentPage) {
+                        $offset = ($currentPage - 1) * 10;
+                    } else {
+                        $offset = 0;
+                    }
+                    
+                    $calculatedID = $id + $offset; 
+                ?>
+
+                <tr>
+                    <td class="border px-4 py-2 align-top text-sm">{{$calculatedID;}}</td>
+                    <td class="border px-4 py-2 align-top text-sm">{{$company->name;}}</td>
+                    <td class="border px-4 py-2 align-top text-sm">{{$company->email;}}</td>
+                    <td class="border px-4 py-2 align-top text-sm">
+                        @if ($company->logo)
+                            <img src="{{ $company->logo }}" alt="image" class="h-11 h-11"></img>
+                        @endif()
+                    </td>
+                    <td class="border px-4 py-2 align-top text-sm">{{$company->website;}}</td>
+                    <td class="border px-4 py-2 align-top text-sm">
+                        <a onclick="showEditModal()" class="pr-2 cursor-pointer"><i class="far fa-edit"></i></a>
+                        <a onclick="showDeleteModal()" class="cursor-pointer"><i class="far fa-trash-alt"></i></a>
+
+                        <!-- <button id="modal-button" type="button">
+                            Launch demo modal
+                        </button> -->
+                    </td>
+                </tr>
+                @endforeach
+        
+            </table>
+        </div>
 
         @if($companies->count())
             {{ $companies->links() }}
@@ -70,10 +72,26 @@
             <p>There are no companies saved.</p>
         @endif
 
+
         <!-- if the white strip reappears at the bottom of this page check the min height settings again -->
     </div>
 
     <br>
+
+    <!-- on click show the edit modal -->
+    <div class="edit-modal hidden h-screen w-screen flex justify-center items-center fixed inset-0 z-50 bg-gray-800 bg-opacity-50">
+        @include('companies.edit')
+    </div>
+
+    <!-- on click show the create modal -->
+    <div class="create-modal hidden h-screen w-full flex justify-center items-center fixed inset-0 z-50 bg-gray-800 bg-opacity-50">
+        @include('companies.create')
+    </div>
+
+    <!-- on click show the delete modal -->
+    <div class="delete-modal hidden h-screen w-full flex justify-center items-center fixed inset-0 z-50 bg-gray-800 bg-opacity-50">
+        @include('companies.delete')
+    </div>
 
 </x-app-layout>
 
